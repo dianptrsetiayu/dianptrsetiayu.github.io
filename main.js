d341425d5f6036f60aff0c22f5f87abd1af754fa
dianptrsetiayu <152471233+dianptrsetiayu@users.noreply.github.com>
2024-06-06 14:55:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-const audio = new Audio('https://vmirror.imslp.org/files/imglnks/usimg/0/06/IMSLP197806-PMLP02397-3-ClairDeLune-j.mp3');
-const musicPromptWindow = document.getElementByClassName('musicPrompt');
-const playAudioBtn = document.getElementByClassName('playAudioBtn');
-audio.muted = true;
-
-playAudioBtn.onclick = function () {
-  playAudio(audio.muted);
-  setPlayAudioBtnText(audio.muted);
+onload = () =>{
+        document.body.classList.remove("container");
 };
-
-musicPromptWindow.querySelectorAll('button').forEach((item) => {
-  item.onclick = function () {
-    playAudio(item.value === '1');
-    playAudioBtn.disabled = false;
-    musicPromptWindow.remove();
-  };
-});
-
-function playAudio(doPlayAudio) {
-  audio.muted = !doPlayAudio;
-  if (doPlayAudio) {
-    audio.play();
-  }
-  setPlayAudioBtnText(audio.muted);  
-}
-
-function setPlayAudioBtnText(isAudioMuted) {
-  playAudioBtn.innerText = isAudioMuted ? 'Play audio' : 'Mute audio';
-}
